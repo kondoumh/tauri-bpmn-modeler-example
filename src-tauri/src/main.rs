44a63624e0765626350940a5dfdfba84b3353393
kondoumh <kondoumh@gmail.com>
2022-12-18 13:55:36 +0900

    add mobile lib

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,13 +4,14 @@
 )]
 
 fn main() {
-  let context = tauri::generate_context!();
-  tauri::Builder::default()
-    .menu(if cfg!(target_os = "macos") {
-      tauri::Menu::os_default(&context.package_info().name)
-    } else {
-      tauri::Menu::default()
-    })
-    .run(context)
-    .expect("error while running tauri application");
+  // let context = tauri::generate_context!();
+  // tauri::Builder::default()
+  //   .menu(if cfg!(target_os = "macos") {
+  //     tauri::Menu::os_default(&context.package_info().name)
+  //   } else {
+  //     tauri::Menu::default()
+  //   })
+  //   .run(context)
+  //   .expect("error while running tauri application");
+  app::AppBuilder::new().run();
 }
